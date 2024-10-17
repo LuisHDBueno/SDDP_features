26e7f5c71c8e6574a8ff8a1ca525891e6ed01bc9
Luís Henrique Domingues Bueno <105614892+LuisHDBueno@users.noreply.github.com>
2024-10-17 11:13:20 -0300

    Unify the functions with the same node type

--- get_graph.jl
+++ get_graph.jl
@@ -3,49 +3,37 @@ using GLPK
 using SDDP
 
 """
-Function to identify the type of policy graph
-
-If the recovery of the policy graph is not implemented, the function will raise an error.
+Function to check the type of the term in the policy graph.
 
 # Arguments
-- `graph::SDDP.PolicyGraph`: Policy graph to be identified
+- `graph::SDDP.PolicyGraph`: Policy graph to be checked.
 
-# Returns
-- `String`: Type of policy graph
+# Raises
+- `error`: If the term type is not `Int64`.
 """
-function identify_policy_graph(graph::SDDP.PolicyGraph)
-    nodes = graph.nodes
-    node_idx = graph.root_node
-    children = graph.root_children
-    if typeof(children[1].term) == Tuple{Int, Int}
-        error("MarkovianPolicyGraph not implemented")
+function check_type(graph::SDDP.PolicyGraph)
+    term = graph.root_children[1].term
+    type = typeof(term)
+    if type != Int64
+        error("Not implemented for term type: $type")
     end
-
-    list_nodes = []
-    while length(children) > 0
-
-        if node_idx in list_nodes
-            return "cycle"
-        else
-            list_nodes = push!(list_nodes, node_idx)
-        end
-
-        if length(children) > 1
-            error("Non-linear policy graph")
-        end
-
-        node_idx = children[1].term
-        prob = children[1].probability
-
-        children = nodes[node_idx].children
-    end
-    return "linear"
 end
 
-function get_linear_graph(policy_graph::SDDP.PolicyGraph)
+"""
+Function to get the graph from a policy graph.
+
+# Arguments
+- `policy_graph::SDDP.PolicyGraph`: Policy graph to be converted.
+
+# Returns
+- `graph::SDDP.Graph`: Graph converted from the policy graph.
+"""
+function get_graph(policy_graph::SDDP.PolicyGraph)
     root_node = policy_graph.root_node
     nodes = policy_graph.nodes
     
+    check_type(policy_graph)
+
     edges = Vector{Tuple{Pair{Int64, Int64}, Float64}}()
     for node in nodes
         # print node pair separated by a space
@@ -63,21 +51,4 @@ function get_linear_graph(policy_graph::SDDP.PolicyGraph)
     )
     return graph
     
-end
-
-function get_cycle_linear_graph(graph::SDDP.PolicyGraph)
-    println(identify_policy_graph(graph))
-end
-
-
-function get_graph(graph::SDDP.PolicyGraph)
-    type = identify_policy_graph(graph)
-
-    if type == "linear"
-        get_linear_graph(graph)
-
-    elseif type == "cycle"
-        get_cycle_linear_graph(graph)
-    end
-
 end
--- test.jl
+++ test.jl
@@ -1,4 +1,5 @@
 include("get_graph.jl")
+using Ipopt
 
 # teste policy graph
 function test_linear_policy_graph()
@@ -20,7 +21,49 @@ function test_linear_policy_graph()
         end
     end
     
-    get_graph(model)
+    println("\n Linear Policy Graph: \n")
+    show(get_graph(model))
+    println("\n")
+end
+
+function test_cycle_policy_graph()
+    #Create cycle graph
+    graph = SDDP.UnicyclicGraph(0.9, num_nodes = 3)
+
+    # Define the stage problem
+    function stage_problem(stage, node)
+        model = SDDP.Model(() -> Ipopt.Optimizer())  # Passa o otimizador sem o bloco do modelo
+
+        # Defina o problema de otimização para o nó do grafo
+        @variable(model, x >= 0)  # Exemplo de variável
+        @objective(model, Min, x)  # Exemplo de função objetivo
+
+        return model
+    end
+
+    # Create the policy graph
+    model = SDDP.PolicyGraph(stage_problem, graph, lower_bound = 0.0)
+    println("\n Cycle Policy Graph: \n")
+    show(get_graph(model))
+    println("\n")
+end
+
+function test_markovian_policy_graph()
+    model = SDDP.MarkovianPolicyGraph(;
+           transition_matrices = [ones(1, 1), [0.5 0.5], [0.8 0.2; 0.2 0.8]],
+           lower_bound = 0.0,
+       ) do sp, node
+        @variable(sp, x >= 0)  # Quantidade produzida
+        @variable(sp, u >= 0)  # Quantidade est
+        @constraint(sp, u >= 10 - x)  # Demanda a ser atendida
+
+        @stageobjective(sp, 2*x + u)  # Função objetivo para minimizar custos
+    end
+    println("\n MarkovianPolicyGraph: \n")
+    show(get_graph(model))
+    println("\n")
 end
 
 test_linear_policy_graph()
+test_cycle_policy_graph()
+test_markovian_policy_graph()
